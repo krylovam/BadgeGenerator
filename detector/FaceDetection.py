f2ecc2822e329ef1eb0b9f22b860a1a55d6c498f
Mikhail Zhelezin <mikhail.zhelezin@yandex.ru>
2022-03-26 16:11:49 +0300

    Add worked tests

--- detector/FaceDetection.py
+++ detector/FaceDetection.py
@@ -1,17 +1,19 @@
 import cv2
-
+import os
 
 class FaceDetector:
     def __init__(self, path):
-        self.face_cascade = cv2.CascadeClassifier('detector/utils/cascade.xml')
+        dir_path = os.path.dirname(__file__)
+        self.face_cascade = cv2.CascadeClassifier(f'{dir_path}/utils/cascade.xml')
         self.img = cv2.imread(path)
         self.gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+
     def detect(self):
-        self.faces = self.face_cascade.detectMultiScale(self.gray, 1.1, 7)
-    
+        self.faces = self.face_cascade.detectMultiScale(self.gray, 1.1, 12)
+
     def show_img(self):
         for (x, y, w, h) in self.faces:
-            cv2.rectangle(self.img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.rectangle(self.img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         cv2.imshow('img', self.img)
         cv2.waitKey()
 
@@ -19,7 +21,10 @@ class FaceDetector:
         assert len(self.faces) == 1
         return self.faces[0]
 
+
 if __name__ == "__main__":
-    detector = FaceDetector('ruslan.jpg')
-    detector.detect()
-    detector.show_img()
+    for path in ['christine_peterson.jpeg', 'judy_estrin.jpeg', 'tim_oreilly.jpeg', 'vint_cerf.jpeg']:
+        detector = FaceDetector(f'/Users/mzhelezin/Documents/projectLFMS/tests/assets/{path}')
+        detector.detect()
+        detector.show_img()
+
